4d8c97d01e1577161bf15c7dc99cf84adf2b3c03
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-23 15:16:00 -0300

    f

---
+++ src/module4/testHTML.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const filePath = path.join(__dirname, 'test.html');
+
+app.get('/', (req: Request, res: Response): void => {
+    fs.readFile(filePath, 'utf8', (err: NodeJS.ErrnoException | null, data: string): void => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        res.header('Content-Type', 'text/html');
+        res.send(data);
+    });
+});
+
+app.listen(port, (): void => {
+    console.log(`Server is running at http://localhost:${port}`);
+});

13e046826f945e39eeb752f11d40c419a54734a4
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-31 11:06:05 -0300

    idk what im doing smile

--- src/module4/createPath.ts
+++ src/module4/createPath.ts
@@ -1,30 +1,46 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import fs from "fs/promises";
 
 const application = express();
 const port = process.env.PORT || 3000;
 
 export class startServer {
 
-    private serverInit (): void {
+    private async fileTest (filePath: string): Promise <void> {
+        try {
 
+            const stat = await fs.stat(filePath);
+            if (stat.isDirectory()) console.log(`${filePath} is a directory`);
+
+        } catch (e) {
+            console.error(`Failed to start the server, ${e}`);
+        }
+    }
+
+    private async serverInit (): Promise <void> {
+        const pathInfo = path.join(__dirname, 'templates/index.html');
+        await this.fileTest(pathInfo);
         application.get('/', (req: Request, res: Response): void => {
 
-            const gatheringPath = path.join(__dirname, 'templates/index.html');
-            res.sendFile(`${gatheringPath}`);
-        
-            // O send manda somente as mensagens. O "sendFile" pode renderizar o arquivo.
-        
-        
+            res.sendFile(`${pathInfo}`);
+
         });
     }
     
-    public displayServer (): void {
+    public async displayServer (): Promise <void> {
 
-        application.listen(port, (): void => {
-            this.serverInit();
-            console.log(`Server running at http://localhost:${port}`);
-        });
+        try {
+
+            await this.serverInit();
+            application.listen(port, (): void => {
+                console.log(`Our server is at: http://localhost:${port}`);
+            });
+
+        } catch (error) {
+            console.error(`A ${error} happened!`);
+            throw new Error ("Something went wrong");
+        };
 
     }
 
@@ -32,3 +48,4 @@ export class startServer {
 
 const gatherInfo = new startServer();
 gatherInfo.displayServer();
+

8f4ed5dd6de01d8c678bc7dbd2ba5bb732a963dd
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-09-02 10:13:57 -0300

    finished app

--- src/module/POST2.ts
+++ src/module/POST2.ts
@@ -16,11 +16,21 @@ export class wholeServer {
 
     private validateEntrance (req: Request, res: Response, next: NextFunction): void {
 
+        const auth = req.headers['authorization'];
+        if (auth !== 'pcsiqueiraALIVE') {
+            res.send("He is NOT alive. REVIVE HIMkkkkkkkkk.");
+            return;
+        };
+
+        next();
+
     }
 
     private serverInit (): void {
 
         this.adjustExpress();
+        
+        app.use('/restrictpage.html', this.validateEntrance);
 
         const pathHome = path.join(__dirname, 'discoverID/homepage.html');
         const pathAbout = path.join(__dirname, 'discoverID/aboutpage.html');
@@ -30,12 +40,50 @@ export class wholeServer {
             res.sendFile(pathHome);
         });
 
+        app.get('/homepage.html', (req: Request, res: Response): void => {
+            res.sendFile(pathHome);
+        });
+
         app.get('/aboutpage.html', (req: Request, res: Response): void => {
             res.sendFile(pathAbout);
         });
 
         app.get('/restrictpage.html', (req: Request, res: Response): void => {
             res.sendFile(pathRestrict);
+        });
+
+        app.post('/homepage.html/sendinformation', (req: Request, res: Response): void => {
+            
+            const user = req.body.user;
+            const username = req.body.username;
+            const password = req.body.password;
+
+            if (!user && !username) {
+                res.send(`Envia as informações direito inseto !!`);
+                return;
+            };
+
+            if (!password && password.value <= 5) {
+                res.send(`Maior ou igual a cinco caracteres burro`);
+                return;
+            };
+
+            res.send(`We received: ${user}, ${username} and ${password}`);
+
+        });
+
+        app.post('/aboutpage.html/sendinfo', (req: Request, res: Response): void => {
+            
+            const info = req.body.info;
+            
+            if (!info && info.length <= 0) {
+                res.send(`Cê não mando nada né misera`);
+                return;
+            };
+
+            console.log(`The user sent: ${info}`);
+            res.send(`Your info was sent to the system. Thanks!`);
+
         });
 
     }

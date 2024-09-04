2f9b47cdf5d4114635b3b7d17a2ad2c107584acc
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-09-04 10:51:26 -0300

    working with partials

--- src/module6/ts/joker.ts
+++ src/module6/ts/joker.ts
@@ -2,18 +2,21 @@ import express from "express";
 import path from "path";
 import { engine } from "express-handlebars";
 import { homepage } from "./mooray";
+import { aboutpage } from "./mooray";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.engine('handlebars', engine ({
     defaultLayout: 'main',
+    partialsDir: path.join(__dirname, '../views/partials'),
 }));
 
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '../views'));
 
 app.get('/', homepage);
+app.get('/about', aboutpage);
 
 app.listen(port, (): void => {
     console.log(`http://localhost:${port}`);
--- src/module6/ts/mooray.ts
+++ src/module6/ts/mooray.ts
@@ -4,7 +4,33 @@ export const homepage = (req: Request, res: Response): void => {
     
     let target = ['ID', 'ID2', 'ID3'];
 
+    const userInfo = {
+        username: 'Joker',
+        password: '12345'
+    };
+
     const authentication = false;
-    res.render('joker', {authentication, target});
-    
+    res.render('joker', {authentication, target, userInfo});
+
+};
+
+export const aboutpage = (req: Request, res: Response): void => {
+
+    const pages = [
+        {
+            username: 'Joker',
+            password: '12345',            
+        },
+        {
+            username: 'Batman',
+            password: '12ddeqqsa',
+        },
+        {
+            username: 'Superman',
+            password: '1234xwre',
+        },
+    ];
+
+    res.render('aboutpage', {pages});
+
 };

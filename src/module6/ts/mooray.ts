7845481433b9f9a0556f7207b9a3914377358583
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-09-04 09:41:10 -0300

    each loop handlebars

--- src/module6/ts/mooray.ts
+++ src/module6/ts/mooray.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 
 export const homepage = (req: Request, res: Response): void => {
+    
+    let target = ['ID', 'ID2', 'ID3'];
+
     const authentication = false;
-    res.render('joker', {authentication});
+    res.render('joker', {authentication, target});
+    
 };

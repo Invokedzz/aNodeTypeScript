0a4c2447dabe57a0b5d72002e8cc9639d4c49440
Samuel Nóbrega <162361529+Invokedzz@users.noreply.github.com>
2024-09-14 11:31:21 -0300

    adding dotenv

--- src/module7/routes.ts
+++ src/module7/routes.ts
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "sss38c197333",
+    password: "",
     database: "bookstore",
     waitForConnections: true,
     connectionLimit: 10,
